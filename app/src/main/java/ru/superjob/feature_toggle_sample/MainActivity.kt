3a96079b2468c5d70e6d035382fbfb686d49e1f8
Игорь Дубровин <i.dubrovin@superjob.ru>
2020-09-20 18:07:16 +0300

    initial

---
+++ app/src/main/java/ru/superjob/feature_toggle_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.superjob.feature_toggle_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
